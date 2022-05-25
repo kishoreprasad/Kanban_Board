0c0ff339186244f922b4c71386e239fd3734e364
Narendranath <rsnarendranath@gmail.com>
2022-05-25 16:16:27 +0530

    popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
-
-import React, { Component } from 'react'
+import React, { Component,useState } from 'react'
 import Board from 'react-trello'
+import { Modal, Button } from "react-bootstrap";
 
 const data = require('./data.json')
 
@@ -18,8 +18,15 @@ const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
   console.log(`targetLaneId: ${targetLaneId}`)
 }
 
+//popup
+//popup
+
 class App extends Component {
-  state = { boardData: { lanes: [] } }
+
+
+  state = { boardData: { lanes: [] },showModal:false }
+  
+
 
   setEventBus = (eventBus) => {
     this.setState({ eventBus })
@@ -50,7 +57,17 @@ class App extends Component {
     console.log(laneId);
   }
   handleCardClick=(props)=>{
+      this.setState({showModal:true})
   }
+
+  handleClose=(props)=>{
+    this.setState({showModal:false})
+  }
+
+  handleShow=(props)=>{
+    this.setState({showModal:true})
+  }
+
   handedelete=(props)=>{
 
     alert(props)
@@ -63,6 +80,29 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h3>MX Kanban</h3>
+        {/* <popup opens */}
+        <>
+          <div
+            className="d-flex align-items-center justify-content-center"
+            style={{ height: "100vh" }}
+          >
+          </div>
+          <Modal show={this.state.showModal} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={this.handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+        {/* Popup Closes */}
         </div>
         <div className="App-intro">
           <Board
